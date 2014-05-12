5a2a7d8e24c4235a91229102775e49412d85c724
klquast <klquast8807@gmail.com>
2014-05-12 17:15:02 -0500

    Style for current leader

--- public/javascripts/mcisCup/js.js
+++ public/javascripts/mcisCup/js.js
@@ -11,15 +11,16 @@ function initViewModel(){
 
 function refreshData(){
     ajax.info().done(function(data){
-	ko.mapping.fromJS(data.competitions, {}, viewModel.competitions)	
-	ko.mapping.fromJS(data.results, {}, viewModel.results)
-	viewModel.teams.removeAll()
-	$.each(data.teams, function(index, team){
-	   viewModel.teams.push(new Team(team.teamId, team.name, team.captainName))
-	})
-	viewModel.teams.sort(function(left, right){
-	    return left.totalPoints() == right.totalPoints() ? 0 : (left.totalPoints() > right.totalPoints() ? -1 : 1)
-	})
+        ko.mapping.fromJS(data.competitions, {}, viewModel.competitions)
+        ko.mapping.fromJS(data.results, {}, viewModel.results)
+        viewModel.teams.removeAll()
+        $.each(data.teams, function(index, team){
+           viewModel.teams.push(new Team(team.teamId, team.name, team.captainName))
+        })
+        viewModel.teams.sort(function(left, right){
+            return left.totalPoints() == right.totalPoints() ? 0 : (left.totalPoints() > right.totalPoints() ? -1 : 1)
+        })
+        viewModel.leader(viewModel.teams()[0])
     })
 }
 
@@ -27,6 +28,7 @@ var viewModel = {
     teams: ko.observableArray([]),
     competitions: ko.observableArray([]),
     results: ko.observableArray([]),
+    leader: ko.observable()
 }
 
 
@@ -36,27 +38,26 @@ function Team(teamId, name, captainName) {
     me.name = ko.observable(name)
     me.captainName = ko.observable(captainName)
     me.results = ko.computed(function(){
-	var list = []
-	$.each(viewModel.competitions(), function(i, c){
-	    var result = { hasValue: ko.observable(false) }
-	    $.each(viewModel.results(), function(j, r){
-		if(r.competitionId() === c.competitionId() && r.teamId() == teamId){
-		    result = { hasValue: ko.observable(true), result: r }
-		}
-	    })
-	    list.push(result)
-	})
-	return list
+        var list = []
+        $.each(viewModel.competitions(), function(i, c){
+            var result = { hasValue: ko.observable(false) }
+            $.each(viewModel.results(), function(j, r){
+                if(r.competitionId() === c.competitionId() && r.teamId() == teamId){
+                    result = { hasValue: ko.observable(true), result: r }
+                }
+            })
+            list.push(result)
+        })
+        return list
     })
     me.totalPoints = ko.computed(function(){
-	var counter = 0;
-	$.each(me.results(), function(i, r){
-	    if(r.hasValue())
-		counter += r.result.pointsEarned()
-	})
-	return counter;
+        var counter = 0;
+        $.each(me.results(), function(i, r){
+            if(r.hasValue())
+            counter += r.result.pointsEarned()
+        })
+        return counter;
     })
-
 }
 
 
