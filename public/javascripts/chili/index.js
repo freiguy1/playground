241e92dc99f394e060d668bb0aee56bce0267174
klquast <klquast8807@gmail.com>
2014-08-29 17:47:12 -0500

    Voting!

--- public/javascripts/chili/index.js
+++ public/javascripts/chili/index.js
@@ -3,11 +3,16 @@ var viewModel = new ChiliViewModel();
 $(function(){
     viewModel.initialize();
     ko.applyBindings(viewModel);
+
+    $('.modal').on('hidden.bs.modal', function () {
+        viewModel.thankYou(false);
+    })
 });
 
 function ChiliViewModel() {
     var self = this;
     self.loading = ko.observable(true);
+    self.thankYou = ko.observable(false);
     self.entries = ko.observableArray([]);
     self.initialize = function(){
         self.loading(true);
@@ -23,7 +28,35 @@ function ChiliViewModel() {
             self.loading(false);
         });
     };
-    self.spiciness = ko.computed(function(){
-        return '-chili';
-    }, this);
+    self.voteFor = ko.observable(new Entry('', '', '', ''));
+
+    self.setVoteFor = function(entry) {
+        self.voteFor(new Entry(entry.name(), entry.number(), '', ''));
+    };
+
+    self.submitVote = function() {
+        var voteInfo = ko.toJS(self.voteFor);
+
+        publicAjax.addVote(voteInfo.entryNumber, voteInfo.voterName, voteInfo.voterComments)
+            .done(function(){
+                self.showThankYou();
+            });
+    };
+
+    self.showThankYou = function(){
+        self.thankYou(true);
+        setTimeout(self.hideThankYou, 2000);
+    };
+
+    self.hideThankYou = function(){
+        $('.modal').modal('hide');
+    };
+}
+
+function Entry(entryName, entryNumber, voterName, voterComments) {
+    var self = this;
+    self.entryName = ko.observable(entryName);
+    self.entryNumber = ko.observable(entryNumber);
+    self.voterName = ko.observable(voterName);
+    self.voterComments = ko.observable(voterComments);
 }
