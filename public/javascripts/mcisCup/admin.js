1308d5893ddd429708b5f33480ebd6a43a3e3a45
Ethan Frei <ethan.frei@gmail.com>
2015-06-23 20:25:12 +0000

    adding confirmation dialogs for deleting teams and competitions

--- public/javascripts/mcisCup/admin.js
+++ public/javascripts/mcisCup/admin.js
@@ -39,13 +39,13 @@ function initViewModel(){
 
 function refreshData(){
     ajax.info().done(function(data){
-	ko.mapping.fromJS(data.competitions, {}, viewModel.competitions)	
-	ko.mapping.fromJS(data.results, {}, viewModel.results)
-	viewModel.teams.removeAll()
-	$.each(data.teams, function(index, team){
-	   viewModel.teams.push(new Team(team.teamId, team.name, team.captainName))
-	})
-	$('.score-popover').popover()
+        ko.mapping.fromJS(data.competitions, {}, viewModel.competitions)        
+        ko.mapping.fromJS(data.results, {}, viewModel.results)
+        viewModel.teams.removeAll()
+        $.each(data.teams, function(index, team){
+           viewModel.teams.push(new Team(team.teamId, team.name, team.captainName))
+        })
+        $('.score-popover').popover()
     })
 }
 
@@ -58,16 +58,24 @@ var viewModel = {
     addTeamCaptainName: ko.observable(""),
 
     editTeam: ko.observable({
-	name: ko.observable(""),
-	captainName: ko.observable("")
+        name: ko.observable(""),
+        captainName: ko.observable("")
     }),
+
+    confirmModalModel: {
+       text: ko.observable(""),
+       yesText: ko.observable(""),
+       noText: ko.observable(""),
+       onYes: function(){},
+       onNo: function(){}
+    },
     
     addCompetitionName: ko.observable(""),
     addCompetitionInstructions: ko.observable(""),
 
     editCompetition: ko.observable({
-	name: ko.observable(""),
-	instructions: ko.observable("")
+        name: ko.observable(""),
+        instructions: ko.observable("")
     }),
     
     editResultTeam: ko.observable(),
@@ -84,8 +92,8 @@ viewModel.createTeamClicked = function(){
 
 viewModel.handleCreateTeam = function(){
     ajax.addTeam({ name: viewModel.addTeamName(), captainName: viewModel.addTeamCaptainName() }).done(function(){
-	$('#addTeamModal').modal('hide')
-	refreshData()
+        $('#addTeamModal').modal('hide')
+        refreshData()
     })
 }
 
@@ -96,15 +104,23 @@ viewModel.editTeamClicked = function(team){
 
 viewModel.handleEditTeam = function(){
     ajax.editTeam(viewModel.editTeam().teamId, { name: viewModel.editTeam().name(), captainName: viewModel.editTeam().captainName() }).done(function(){
-	$('#editTeamModal').modal('hide')
-	refreshData()
+        $('#editTeamModal').modal('hide')
+        refreshData()
     })
 }
 
 viewModel.deleteTeam = function(team){
-    ajax.deleteTeam(team.teamId).done(function(){
-	refreshData()
-    })
+   showConfirmModal(
+      "Are you sure you want to delete '" + team.name() + "'?",
+      "Yes",
+      "No",
+      function() {
+         $('#confirmModal').modal('hide');
+         ajax.deleteTeam(team.teamId).done(function(){
+             refreshData();
+         });
+      },
+      function() {});
 }
 
 viewModel.createCompetitionClicked = function(){
@@ -115,9 +131,9 @@ viewModel.createCompetitionClicked = function(){
 
 viewModel.handleCreateCompetition = function(){
     ajax.addCompetition({ name: viewModel.addCompetitionName(), instructions: viewModel.addCompetitionInstructions() }).done(function(){
-	$('#addCompetitionModal').modal('hide')    
-	refreshData()
-    })	
+        $('#addCompetitionModal').modal('hide')    
+        refreshData()
+    })  
 }
 
 viewModel.editCompetitionClicked = function(competition){
@@ -127,47 +143,55 @@ viewModel.editCompetitionClicked = function(competition){
 
 viewModel.handleEditCompetition = function(){
     ajax.editCompetition(viewModel.editCompetition().competitionId(), 
-	{ name: viewModel.editCompetition().name(), instructions: viewModel.editCompetition().instructions() })
+        { name: viewModel.editCompetition().name(), instructions: viewModel.editCompetition().instructions() })
     .done(function(){
-	$('#editCompetitionModal').modal('hide')    
-	refreshData()
-    })	
+        $('#editCompetitionModal').modal('hide')    
+        refreshData()
+    })  
 }
 
 viewModel.deleteCompetition = function(comp){
-    ajax.deleteCompetition(comp.competitionId()).done(function(){
-	refreshData()
-    })
+   showConfirmModal(
+      "Are you sure you want to delete '" + comp.name() + "'?",
+      "Yes",
+      "No",
+      function() {
+         $('#confirmModal').modal('hide');
+         ajax.deleteCompetition(comp.competitionId()).done(function(){
+             refreshData()
+         })
+      },
+      function() {});
 }
 
 viewModel.editResultClicked = function(teamId, competitionId){
     $.each(viewModel.competitions(), function(i, c){
-	if(c.competitionId() == competitionId)
-	    viewModel.editResultCompetition(c)
+        if(c.competitionId() == competitionId)
+            viewModel.editResultCompetition(c)
     })
     $.each(viewModel.teams(), function(i, t){
-	if(t.teamId == teamId)
-	    viewModel.editResultTeam(t)
+        if(t.teamId == teamId)
+            viewModel.editResultTeam(t)
     })
     viewModel.editResultPointsEarned("")
     viewModel.editResultNotes("")
     $.each(viewModel.results(), function(i, r){
-	if(r.teamId() == teamId && r.competitionId() == competitionId){
-	    viewModel.editResultPointsEarned(r.pointsEarned())
-	    viewModel.editResultNotes(r.notes())
-	}
+        if(r.teamId() == teamId && r.competitionId() == competitionId){
+            viewModel.editResultPointsEarned(r.pointsEarned())
+            viewModel.editResultNotes(r.notes())
+        }
     })
     $('#editResultModal').modal('show')
 }
 
 viewModel.handleEditResult = function(){
     ajax.editResult(
-	viewModel.editResultTeam().teamId, 
-	viewModel.editResultCompetition().competitionId(), 
-	{ pointsEarned: parseInt(viewModel.editResultPointsEarned()), notes: viewModel.editResultNotes() }
+        viewModel.editResultTeam().teamId, 
+        viewModel.editResultCompetition().competitionId(), 
+        { pointsEarned: parseInt(viewModel.editResultPointsEarned()), notes: viewModel.editResultNotes() }
     ).done(function(){
-	$('#editResultModal').modal('hide')
-	refreshData()
+        $('#editResultModal').modal('hide')
+        refreshData()
     })
 }
 
@@ -178,27 +202,36 @@ function Team(teamId, name, captainName) {
     me.name = ko.observable(name)
     me.captainName = ko.observable(captainName)
     me.results = ko.computed(function(){
-	var list = []
-	$.each(viewModel.competitions(), function(i, c){
-	    var result = { hasValue: ko.observable(false), result: {teamId: ko.observable(teamId), competitionId: c.competitionId } }
-	    $.each(viewModel.results(), function(j, r){
-		if(r.competitionId() === c.competitionId() && r.teamId() == teamId){
-		    result = { hasValue: ko.observable(true), result: r }
-		}
-	    })
-	    list.push(result)
-	})
-	return list
+        var list = []
+        $.each(viewModel.competitions(), function(i, c){
+            var result = { hasValue: ko.observable(false), result: {teamId: ko.observable(teamId), competitionId: c.competitionId } }
+            $.each(viewModel.results(), function(j, r){
+                if(r.competitionId() === c.competitionId() && r.teamId() == teamId){
+                    result = { hasValue: ko.observable(true), result: r }
+                }
+            })
+            list.push(result)
+        })
+        return list
     })
     me.totalPoints = ko.computed(function(){
-	var counter = 0;
-	$.each(me.results(), function(i, r){
-	    if(r.hasValue())
-		counter += r.result.pointsEarned()
-	})
-	return counter;
+        var counter = 0;
+        $.each(me.results(), function(i, r){
+            if(r.hasValue())
+                counter += r.result.pointsEarned()
+        })
+        return counter;
     })
 
+}
+
+function showConfirmModal(text, yesText, noText, onYes, onNo) {
+   viewModel.confirmModalModel.text(text);
+   viewModel.confirmModalModel.yesText(yesText);
+   viewModel.confirmModalModel.noText(noText);
+   viewModel.confirmModalModel.onYes = onYes;
+   viewModel.confirmModalModel.onNo = onNo;
+   $('#confirmModal').modal('show');
 }
 
 
@@ -206,62 +239,62 @@ function Team(teamId, name, captainName) {
 
 var ajax = {
     info : function() {
-	return $.ajax({
-	    type: "GET",
-	    url: "/mcisCup/info"
-	})
+        return $.ajax({
+            type: "GET",
+            url: "/mcisCup/info"
+        })
     },
     addTeam: function(team) {
-	return $.ajax({
-	    type: "POST",
-	    url: "/mcisCup/teams",
-	    data: JSON.stringify(team),
-	    contentType: "application/json"
-	})
+        return $.ajax({
+            type: "POST",
+            url: "/mcisCup/teams",
+            data: JSON.stringify(team),
+            contentType: "application/json"
+        })
     },
     editTeam: function(teamId, team) {
-	return $.ajax({
-	    type: "PUT",
-	    url: "/mcisCup/teams/" + teamId,
-	    data: JSON.stringify(team),
-	    contentType: "application/json"
-	})
+        return $.ajax({
+            type: "PUT",
+            url: "/mcisCup/teams/" + teamId,
+            data: JSON.stringify(team),
+            contentType: "application/json"
+        })
     },
     deleteTeam: function(teamId) {
-	return $.ajax({
-	    type: "DELETE",
-	    url: "/mcisCup/teams/" + teamId
-	})
+        return $.ajax({
+            type: "DELETE",
+            url: "/mcisCup/teams/" + teamId
+        })
     },
     addCompetition: function(competition) {
-	return $.ajax({
-	    type: "POST",
-	    url: "/mcisCup/competitions",
-	    data: JSON.stringify(competition),
-	    contentType: "application/json"
-	})
+        return $.ajax({
+            type: "POST",
+            url: "/mcisCup/competitions",
+            data: JSON.stringify(competition),
+            contentType: "application/json"
+        })
     },
     editCompetition: function(competitionId, competition) {
-	return $.ajax({
-	    type: "PUT",
-	    url: "/mcisCup/competitions/" + competitionId,
-	    data: JSON.stringify(competition),
-	    contentType: "application/json"
-	})
+        return $.ajax({
+            type: "PUT",
+            url: "/mcisCup/competitions/" + competitionId,
+            data: JSON.stringify(competition),
+            contentType: "application/json"
+        })
     },
     deleteCompetition: function(competitionId) {
-	return $.ajax({
-	    type: "DELETE",
-	    url: "/mcisCup/competitions/" + competitionId
-	})
+        return $.ajax({
+            type: "DELETE",
+            url: "/mcisCup/competitions/" + competitionId
+        })
     },
     editResult: function(teamId, competitionId, result) {
-	return $.ajax({
-	    type: "PUT",
-	    url: "/mcisCup/competitions/" + competitionId + "/teams/" + teamId,
-	    data: JSON.stringify(result),
-	    contentType: "application/json"
-	})
+        return $.ajax({
+            type: "PUT",
+            url: "/mcisCup/competitions/" + competitionId + "/teams/" + teamId,
+            data: JSON.stringify(result),
+            contentType: "application/json"
+        })
     }
 }
 
