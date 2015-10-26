e494c82ec015d8c94adbe86d3f7791c44c1a4728
Ethan Frei <ethan.frei@gmail.com>
2015-10-26 18:22:35 +0000

    fixing problem with mariadb 256 char long unique column

--- conf/evolutions/default/6.sql
+++ conf/evolutions/default/6.sql
@@ -2,7 +2,7 @@
 
 CREATE TABLE authenticator (
     authenticatorId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    identifier VARCHAR(256) UNIQUE NOT NULL,
+    identifier VARCHAR(255) UNIQUE NOT NULL,
     userId VARCHAR(64) NOT NULL,
     providerId VARCHAR(64) NOT NULL,
     creationDate TIMESTAMP NOT NULL,
